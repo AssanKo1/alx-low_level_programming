9db03be8851c1b6851a1d8e0b6f7d369a770862d
AssanKo <kojoassan75@gmail.com>
2022-09-09 23:22:37 -0700

    My first C language

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,10 +3,11 @@
 /***
  * main - prints all the numbers of base 16 in lowercase,
  * followed by a new line
- * Return: Always 0 (Success)
+ * Return:Always 0 (Success)
  */
 
 int main(void)
+
 {
 int n;
 char ch;
