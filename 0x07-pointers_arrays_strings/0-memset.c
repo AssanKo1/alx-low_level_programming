ec04c2d1b880f969c0b7032afdf09377c3c5c9d6
AssanKo <kojoassan75@gmail.com>
2022-09-27 16:52:43 -0700

    My first C language

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,14 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
- * Return: pointer to the memory area s
+ * _memset - Function that fills memory with a constant byte
+ * @s: Pointer parameter
+ * @b: character parameter
+ * @n: integer parameter
+ * Return: return (0) or some character
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
-for (i = 0; i < n; i++)
-{
-s[i] = b;
-}
-return (s);
+return (memset(s, b, n,));
 }
