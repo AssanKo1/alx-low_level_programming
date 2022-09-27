860850b8a9c964dcdafa8e688025c5fc0f067a38
AssanKo <kojoassan75@gmail.com>
2022-09-27 16:38:55 -0700

    My first C language

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 fndef FILE_HOLBERTON
 #define FILE_HOLBERTON
+
 #int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -10,5 +11,6 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+
 #endif
 
